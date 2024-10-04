d409f571c8a6ccf816f3140770933d0a89941b44
Yooshyasha <maksimmorozov962@gmail.com>
2024-10-04 14:06:17 +0500

    add json_answer

---
+++ sampletelegrambot/src/core/json_answer.py
@@ -0,0 +1,21 @@
+import aiofiles
+import json
+
+from typing import Optional
+
+
+class JsonAnswer:
+    @staticmethod
+    async def get(*tree: str) -> Optional[str]:
+        try:
+            async with aiofiles.open("data/answers.json") as file:
+                data = json.load(file)
+                for name in tree:
+                    data = data.get(name)
+                    if data is None:
+                        return None
+                if isinstance(data, str):
+                    return data
+        except (FileNotFoundError, json.JSONDecodeError):
+            return None
+        return None
