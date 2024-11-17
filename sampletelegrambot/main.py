8777450b39eab103ce8b770cc1c3390d95b0272c
Yooshyasha <mejzikrejzi66@gmail.com>
2024-11-18 00:03:40 +0500

    chore

--- sampletelegrambot/main.py
+++ sampletelegrambot/main.py
@@ -5,19 +5,6 @@ from tortoise import Tortoise
 from sampletelegrambot.src.app.bot.bot import start_pooling
 from sampletelegrambot.src.core.config import TORTOISE_ORM
 
-from aerich import Command
-
-from y24fw.logger.logger import logger
-
-
-async def migration():
-    try:
-        migration_command = Command(tortoise_config=TORTOISE_ORM)
-        await migration_command.init()
-        await migration_command.migrate()
-    except Exception as ex:
-        logger.error(ex)
-
 
 async def main():
     logger.info("Loading...")
