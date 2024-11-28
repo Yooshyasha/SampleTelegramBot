fbc42751cb1e52cb90c1b37798c1d8a2cd59bb29
Yooshyasha <mejzikrejzi66@gmail.com>
2024-11-28 10:00:08 +0500

    ref

--- sampletelegrambot/src/app/bot/modules/middlewares.py
+++ sampletelegrambot/src/app/bot/modules/middlewares.py
@@ -14,13 +14,8 @@ class UserMiddleware(BaseMiddleware):
                        handler: Callable[[TelegramObject, Dict[str, Any]], Awaitable[Any]], 
                        event: TelegramObject, 
                        data: Dict[str, Any]) -> Any:
-        
-        data["user_first_start"] = False
-        
-        if await User.get_or_none(tg_id=event.from_user.id) is None:
-            data["user_first_start"] = True
-        
-        data["user"] = await User.get_or_create(tg_id=event.from_user.id)
-        data["user"] = data["user"][0]
+
+        user, created = await User.get_or_create(tg_id=event.from_user.id)
+        data.update({"user": user, "user_first_start": created})
 
         return await handler(event, data)
