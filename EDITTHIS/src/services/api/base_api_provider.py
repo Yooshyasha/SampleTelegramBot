778416912d21509a74596dba195a167d3596302d
Yooshyasha <mejzikrejzi66@gmail.com>
2025-02-09 20:14:11 +0500

    feat: API provider

---
+++ EDITTHIS/src/services/api/base_api_provider.py
@@ -0,0 +1,23 @@
+from abc import abstractmethod
+
+from EDITTHIS.src.core.utils import SingletonABCMeta
+
+
+class BaseAPIProvider(metaclass=SingletonABCMeta):
+    """Класс, для инкапсулирования логики, связанной со сторонними API"""
+    _is_initialized: bool = False
+
+    async def initialize(self):
+        """Инициализация API провайдера"""
+        if not self._is_initialized:
+            self._is_initialized = True
+            return await self._initialize()
+
+    @abstractmethod
+    async def _initialize(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    async def destroy(self):
+        """Метод, в котором будут проходить запросы, связанные с дисконнектом от API."""
+        raise NotImplementedError
